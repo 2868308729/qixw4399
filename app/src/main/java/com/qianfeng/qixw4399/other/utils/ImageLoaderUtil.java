ef9978037f124b21163303162bc9b79b0d2b632b
2868308729 <2868308729@qq.com>
2016-02-22 16:51:43 +0800

    mess

--- app/src/main/java/com/qianfeng/qixw4399/other/utils/ImageLoaderUtil.java
+++ app/src/main/java/com/qianfeng/qixw4399/other/utils/ImageLoaderUtil.java
@@ -1,9 +1,15 @@
 package com.qianfeng.qixw4399.other.utils;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.LruCache;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.qianfeng.qixw4399.R;
 
 /**
@@ -11,6 +17,18 @@ import com.qianfeng.qixw4399.R;
  * on  16-2-16.
  */
 public class ImageLoaderUtil {
+
+    private ImageLoaderUtilCallBack callBack;
+    private ImageLoaderUtil(){}
+    public ImageLoaderUtil getInstance(){
+        return new ImageLoaderUtil();
+    }
+    interface  ImageLoaderUtilCallBack{
+        public void callBack(Bitmap bitmap);
+    }
+    public void setImageLoaderUtilCallBack(ImageLoaderUtilCallBack callBack){
+        this.callBack = callBack;
+    }
     /**
      * 加载图片
      * @param imageView
@@ -39,6 +57,31 @@ public class ImageLoaderUtil {
                 R.mipmap.ic_launcher,
                 R.mipmap.ic_launcher);
         imageLoader.get(url, listener);
+    }
+
+    /**
+     * 图片请求
+     * @param url
+     * @param view
+     * @param width
+     * @param height
+     */
+    private void imageRequest(String url, final View view, int width, int height){
+        ImageRequest imageRequest = new ImageRequest(
+                url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                       callBack.callBack(response);
+                    }
+                }, width, height, ImageView.ScaleType.CENTER, Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        LogUtil.d("tag","图片请求失败："+error.toString());
+                    }
+                });
+        MyApplication.requestQueue.add(imageRequest);
     }
 
 
